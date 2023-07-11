2b685727c40d96f53a2e6d7da6b4a9cad826c402
mauriciomaniglia <manigliamauricio@gmail.com>
2023-07-10 22:26:01 -0300

    Move WebViewRule enum to its own file

--- Sources/core-web-browser/WebView/WebViewProxy.swift
+++ Sources/core-web-browser/WebView/WebViewProxy.swift
@@ -1,28 +1,5 @@
 import WebKit
 
-public enum WebViewRule: String {
-    case advertising
-    case analytics
-    case social
-    case cryptomining
-    case fingerprinting
-
-    func content() -> String {
-        switch self {
-        case .advertising:
-            return blockAdvertising
-        case .analytics:
-            return blockAnalytics
-        case .social:
-            return blockSocial
-        case .cryptomining:
-            return blockCryptomining
-        case .fingerprinting:
-            return blockFingerprinting
-        }
-    }
-}
-
 public protocol WebViewProxyProtocol {
     func didLoadPage()
     func didUpdateLoadingProgress(_ progress: Double)
