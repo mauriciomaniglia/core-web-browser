e70ed17d7889c93dbebf749d4cbafd6a884dd5ef
mauriciomaniglia <manigliamauricio@gmail.com>
2023-08-03 08:56:46 -0300

    Add rules provider for ContentBlocking

--- Tests/core-web-browserTests/ContentBlocking/ContentBlockingTests.swift
+++ Tests/core-web-browserTests/ContentBlocking/ContentBlockingTests.swift
@@ -1,29 +1,41 @@
 import XCTest
 import core_web_browser
 
+protocol RulesProviderContract {}
+
 final class ContentBlocking {
     private let webView: WebViewContract
+    private let rulesProvider: RulesProviderContract
 
-    init(webView: WebViewContract) {
+    init(webView: WebViewContract, rulesProvider: RulesProviderContract) {
         self.webView = webView
+        self.rulesProvider = rulesProvider
     }
 }
 
 class ContentBlockingTests: XCTestCase {
 
     func test_init_doesNotSendAnyMessages() {
-        let (_, webView) = makeSUT()
+        let (_, webView, rulesProvider) = makeSUT()
 
         XCTAssertEqual(webView.receivedMessages, [])
+        XCTAssertEqual(rulesProvider.receivedMessages, [])
     }
 
 
     // MARK: - Helpers
 
-    private func makeSUT() -> (sut: ContentBlocking, webView: WebViewSpy) {
+    private func makeSUT() -> (sut: ContentBlocking, webView: WebViewSpy, rulesProvider: RulesProviderSpy) {
         let webView = WebViewSpy()
-        let sut = ContentBlocking(webView: webView)
+        let rulesProvider = RulesProviderSpy()
+        let sut = ContentBlocking(webView: webView, rulesProvider: rulesProvider)
 
-        return (sut, webView)
+        return (sut, webView, rulesProvider)
     }
+}
+
+private class RulesProviderSpy: RulesProviderContract {
+    enum Message: Equatable {}
+
+    var receivedMessages = [Message]()
 }
