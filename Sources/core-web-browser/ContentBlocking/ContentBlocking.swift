e3cee6c544453501ac6aabc43258c9628afcbc49
mauriciomaniglia <manigliamauricio@gmail.com>
2023-09-25 13:50:15 -0300

    Move RuleProviderContract enum to its own file

--- Sources/core-web-browser/ContentBlocking/ContentBlocking.swift
+++ Sources/core-web-browser/ContentBlocking/ContentBlocking.swift
@@ -1,14 +1,3 @@
-public protocol RulesProviderContract {
-    func advertising() -> (name: String, content: String)
-    func analytics() -> (name: String, content: String)
-    func social() -> (name: String, content: String)
-    func cryptomining() -> (name: String, content: String)
-    func fingerprinting() -> (name: String, content: String)
-    func advertisingCookies() -> (name: String, content: String)
-    func analyticsCookies() -> (name: String, content: String)
-    func socialCookies() -> (name: String, content: String)
-}
-
 final public class ContentBlocking {
     private let webView: WebViewContract
     private let rulesProvider: RulesProviderContract
