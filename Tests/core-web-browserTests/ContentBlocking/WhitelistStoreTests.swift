5f5e2b7e677dc8be355826959a76fcd662bc7325
mauriciomaniglia <manigliamauricio@gmail.com>
2023-11-14 18:01:49 -0300

    Test saveDomain when list is empty, saves the new domain

--- Tests/core-web-browserTests/ContentBlocking/WhitelistStoreTests.swift
+++ Tests/core-web-browserTests/ContentBlocking/WhitelistStoreTests.swift
@@ -3,13 +3,30 @@ import core_web_browser
 
 class WhitelistStore {
     static func isRegisteredDomain(_ domain: String) -> Bool {
-        return false
+        let whitelist = UserDefaults.standard.stringArray(forKey: "Whitelist") ?? []
+
+        return whitelist.contains(domain)
+    }
+
+    static func saveDomain(_ domain: String) {
+        UserDefaults.standard.set([domain], forKey: "Whitelist")
     }
 }
 
 class WhitelistStoreTests: XCTestCase {
+    override func tearDown() {
+        UserDefaults.standard.set([], forKey: "Whitelist")
+    }
 
     func test_isRegisteredDomain_whenListIsEmptyReturnsFalse() {
         XCTAssertFalse(WhitelistStore.isRegisteredDomain("www.apple.com"))
+    }
+
+    func test_saveDomain_whenListIsEmptySavesTheNewDomain() {
+        XCTAssertFalse(WhitelistStore.isRegisteredDomain("www.apple.com"))
+
+        WhitelistStore.saveDomain("www.apple.com")
+
+        XCTAssertTrue(WhitelistStore.isRegisteredDomain("www.apple.com"))
     }
 }
