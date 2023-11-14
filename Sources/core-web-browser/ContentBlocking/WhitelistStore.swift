45c75165a610485c6c10cc4f0207eb3c0bd21340
mauriciomaniglia <manigliamauricio@gmail.com>
2023-11-14 20:02:50 -0300

    Refactor WhitelistStore

--- Sources/core-web-browser/ContentBlocking/WhitelistStore.swift
+++ Sources/core-web-browser/ContentBlocking/WhitelistStore.swift
@@ -2,26 +2,34 @@ import Foundation
 
 public class WhitelistStore {
     public static func isRegisteredDomain(_ domain: String) -> Bool {
-        let whitelist = UserDefaults.standard.stringArray(forKey: "Whitelist") ?? []
+        let whitelist = fetchRegisteredDomains()
 
         return whitelist.contains(domain)
     }
 
     public static func saveDomain(_ domain: String) {
-        var whitelist = UserDefaults.standard.stringArray(forKey: "Whitelist") ?? []
+        var whitelist = fetchRegisteredDomains()
 
         if !whitelist.contains(domain) {
             whitelist.append(domain)
-            UserDefaults.standard.set(whitelist, forKey: "Whitelist")
+            registerDomains(whitelist)
         }
     }
 
     public static func removeDomain(_ domain: String) {
-        var whitelist = UserDefaults.standard.stringArray(forKey: "Whitelist") ?? []
+        var whitelist = fetchRegisteredDomains()
 
         if let index = whitelist.firstIndex(of: domain) {
             whitelist.remove(at: index)
-            UserDefaults.standard.set(whitelist, forKey: "Whitelist")
+            registerDomains(whitelist)
         }
+    }
+
+    private static func fetchRegisteredDomains() -> [String] {
+        UserDefaults.standard.stringArray(forKey: "Whitelist") ?? []
+    }
+
+    private static func registerDomains(_ domains: [String]) {
+        UserDefaults.standard.set(domains, forKey: "Whitelist")
     }
 }
